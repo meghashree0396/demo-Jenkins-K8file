f79072c771542c8f6871e7547b89feb43d849034
meghashree0396 <70893352+meghashree0396@users.noreply.github.com>
2021-03-01 10:10:03 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -6,7 +6,7 @@ app = flask.Flask(__name__)
 @app.route('/')
 @app.route('/hello/')
 def hello_world():
-    return 'Hello world\n'
+    return 'Hello world!\n'
 
 @app.route('/hello/<username>') # dynamic route
 def hello_user(username):
